bec46249370c18ecab81575f6ad46cd1eafb1372
Chrisy <Chrisy@Chrisy-PC>
2015-11-29 15:19:43 +0100

    project pixytest generated

---
+++ pixytest.X/pixytestmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   pixytestmain.c
+ * Author: Chrisy
+ *
+ * Created on 29. November 2015, 15:12
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
