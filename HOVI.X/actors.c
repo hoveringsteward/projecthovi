bcea6ef8237aeed27c497233f0946444f32b1860
Lucas <Lucas@Lucas-PC.lan>
2015-12-25 18:27:25 +0100

    chrisys files Signed-off-by:Lucas <Lucas@Lucas-PC.lan>

---
+++ HOVI.X/actors.c
@@ -0,0 +1,22 @@
+/*-----------------------------------------------------------------*/
+/* Author: Chrisy                                                  */
+/*                                                                 */
+/* Created: 2015-12-25                                             */
+/* Project: HOVI.X                                                 */
+/*                                                                 */
+/* Actors are components like propellers
+ * they have to act, after they get the information from sensors
+ * for example: if the position is wrong, he has to nick
+ * if the rotation is wrong, he has to gear
+ * if everything is fine, he could fly forwards
+ * he has to change the velocity, if he flys to high or to low
+ * -> the commands for roll / nick / gear are in their c-files 
+ * (roll_nick_gear - directory)
+ */
+
+#include <xc.h>
+#include "main.h"
+
+
+
+
