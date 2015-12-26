adba5b25d5f481b3a3b8c5a6f621052568c4ef29
Lucas <Lucas@Lucas-PC.lan>
2015-12-26 17:03:14 +0100

    resolving crashes Signed-off-by:Lucas <Lucas@Lucas-PC.lan>

---
+++ HOVI_PIC16_SPI.X/init.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   init.h
+ * Author: Lucas
+ *
+ * Created on 15. Dezember 2015, 09:57
+ */
+
+#ifndef INIT_H
+#define	INIT_H
+
+#include "main.h"
+
+void Fkt_init(void);
+
+#endif	/* INIT_H */
+
---
+++ HOVI_PIC16_SPI.X/main.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.h
+ * Author: Lucas
+ *
+ * Created on 15. Dezember 2015, 09:55
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#include "init.h"
+#include "spi.h"
+
+#define _XTAL_FREQ 4000000
+
+#endif	/* MAIN_H */
+
