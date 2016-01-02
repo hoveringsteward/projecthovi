a8385fda15e389c37d58ed5f9ad7b219d3feb0ce
chrizzlybaer <c_b@live.at>
2016-01-02 18:34:32 +0100

    commit weil baum

---
+++ HOVI.X/build/default/debug/actors.d
@@ -0,0 +1,1 @@
+ build/default/debug/actors.d  build/default/debug/actors.p1:  actors.c init.h position.h communication.h main.h actors.h safety.h ultrasonic.h gear.h failure.h pixy.h
---
+++ HOVI.X/build/default/debug/failure.d
@@ -0,0 +1,1 @@
+ build/default/debug/failure.d  build/default/debug/failure.p1:  failure.c init.h position.h communication.h main.h actors.h safety.h ultrasonic.h gear.h failure.h pixy.h
